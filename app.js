e0dfccaecbd267f7c239b7fc6c94e3cb979de766
codewithkevin <oseikelvin709@gmail.com>
2022-04-01 17:56:35 +0000

    update

--- app.js
+++ app.js
@@ -8,37 +8,15 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     var today = new Date();
     var currentDay = today.getDay();
-    var time = today.getHours();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday" ;
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     }
+    console.log(options);
 
     res.render("list",
      { kidofDay: day,
-        timezone : time
      });
         
 
